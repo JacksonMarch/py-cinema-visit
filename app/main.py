10dc19174fc8a4e4c8849fe8251edab3394866ad
Yevgeniy Marchenko <Jackson.stethem@gmail.com>
2026-03-29 23:44:30 +0300

    Make wather_cinema. All methods appended.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: str,
+        cleaner: str,
+        movie: str
+) -> None:
+    customer_instances = []
+    cleaner_instance = Cleaner(cleaner)
+    for data in customers:
+        customer = Customer(data["name"], data["food"])
+        customer_instances.append(customer)
+        CinemaBar.sell_product(customer.food, customer)
+
+    hall = CinemaHall(hall_number)
+    hall.movie_session(movie, customer_instances, cleaner_instance)
